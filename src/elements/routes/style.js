0f25d0a9992cfcdd0a128d1d04da83134580ac36
Daniellyfreitasc <daniellyfreitasc@hotmail.com>
2022-04-23 13:32:33 -0300

    Iniciando

---
+++ src/elements/routes/style.js
@@ -0,0 +1,51 @@
+import styled from 'styled-components';
+import Fundocovid from '../imgs/fundocovid.jpg';
+
+export const Container = styled.div`
+	background: url(${Fundocovid}) repeat center;
+	min-width: 100%;
+	min-height: 100vh;
+	background-size: cover;
+	text-align: center;
+	overflow-x: hidden;
+	margin: auto;
+`;
+export const H1 = styled.h1`
+    color: white;
+    width: 40%;
+    font-weight: bolder;
+    padding: 40px;
+    font-size: 62px;
+    text-shadow: 1px 1px 1px white;
+    opacity: 0.9;
+    @media (max-width: 700px) {
+      width: 90%;
+      font-size: 52px;
+      padding: 30px;
+	  }
+`;
+
+export const Span = styled.span`
+  color: black;
+`;
+
+export const Header = styled.header`
+	height: 6vh;
+  overflow: hidden;
+  text-align: left;
+	background-image: linear-gradient(
+		35deg,
+		hsl(210deg 100% 65%) 0%,
+		hsl(209deg 73% 55%) 11%,
+		hsl(207deg 63% 47%) 24%,
+		hsl(206deg 68% 38%) 38%,
+		hsl(205deg 74% 31%) 54%,
+		hsl(204deg 82% 24%) 75%,
+		hsl(203deg 89% 17%) 100%
+	);
+`;
+
+export const Img = styled.img`
+  height: 6vh;
+  margin: 0 2%;
+`
---
+++ src/home/homestyle.js
@@ -0,0 +1,92 @@
+import styled from "styled-components";
+
+export const DivInput = styled.div`
+  width: 90%;
+  margin-left: 5%;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+`
+
+export const DateDados = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+`
+
+export const Paragrafo = styled.p`
+  padding: 2% 0;
+  font-size: 18px;
+  overflow: hidden;
+  @media (max-width: 700px) {
+    font-size: 12px;
+    padding: 2%;
+	}
+`
+export const ButtonComeBack = styled.button`
+	position: absolute;
+	top: 0;
+	left: 45%;
+	margin: 0.4% 0;
+	color: white;
+	font-weight: bolder;
+	background-color: transparent;
+	border: none;
+	cursor: pointer;
+	width: 180px;
+	height: 25px;
+	:hover {
+		transition: 0.5s;
+		font-size: 16px;
+	}
+`;
+
+export const Selects = styled.select`
+    margin: 2% 0;
+    background-color: #ffffff;
+    border: none;
+    padding: 12px 22px;
+    font-size: 16px;
+    border-radius: 10px;
+    color: #212165;
+    font-weight: 900;
+    width: 60%;
+	`
+	
+export const Options = styled.option`
+	font-weight: bolder;
+  color: black;
+`
+export const Button = styled.button`
+    background-color: transparent;
+    padding: 10px 22px;
+    font-size: 22px;
+    border-radius: 25px;
+    width: 250px;
+    margin-left: 5%;
+    color: #ffffff;
+    border: #1b89ae solid 2px;
+    font-weight: 900;
+    &:hover{
+        cursor: pointer;
+        background-color:  #ffffff;
+        color: #0f334c;
+        transition: 0.3s;
+    }
+    &:disabled {
+      color: black;
+      background-color: transparent;
+      border: black solid 2px;
+    }
+    @media (max-width: 700px) {
+      margin: 5% auto;
+	  }
+`
+export const DivInfo = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+`
+export const DadosP = styled.h2`
+  margin: 0 10%;
+`
