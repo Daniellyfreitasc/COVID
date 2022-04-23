79d8f81259745c3f8d43f7df84221fbb300851e9
Daniellyfreitasc <daniellyfreitasc@hotmail.com>
2022-04-23 16:47:22 -0300

    Fim

---
+++ src/elements/urls.js
@@ -0,0 +1,8 @@
+export const SUPABASE_URL = 'https://czdtqehwdhzhdimnayva.supabase.co/rest/v1/COVID'
+
+
+export const SUPABASE_ANON_KEY = {
+  headers: {
+    apikey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6ZHRxZWh3ZGh6aGRpbW5heXZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTA3MzU4MTgsImV4cCI6MTk2NjMxMTgxOH0.5S6fYT-46zkERiIVR6Spg07ff655ExFX7MXCamdlnbo'
+  }
+}
